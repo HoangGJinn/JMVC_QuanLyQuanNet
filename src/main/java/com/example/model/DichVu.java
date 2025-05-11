8d650df7dbe1a1d4011dcbda90135756b3c83fc0
XuanHuy <23110102@student.hcmute.edu.vn>
2025-05-11 23:51:16 +0700

    update model + finish NhanVienForm

--- src/main/java/com/example/model/DichVu.java
+++ src/main/java/com/example/model/DichVu.java
@@ -4,8 +4,8 @@ public class DichVu {
     public String MaDV;
     public String TenDV;
     public ArrayList<ChiTietDV> ChiTietDVs;
-    public DVDoAn dvDoAn;
-    public DVDoUong dvDoUong;
+    public DvDoAn dvDoAn;
+    public DvDoUong dvDoUong;
     public DvTheCao dvTheCao;
     public DichVu() {
         this.ChiTietDVs = new ArrayList<ChiTietDV>();
